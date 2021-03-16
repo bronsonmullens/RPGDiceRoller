3cf1f4500c4c2fedbb712842b6798f1e3ea25e74
Bronson Mullens <bronsonmullens@icloud.com>
2021-03-16 15:02:50 -0700

    Set up dice controller.

---
+++ RPGDiceRoller/Model Controller/DiceController.swift
@@ -0,0 +1,30 @@
+//
+//  DiceController.swift
+//  RPGDiceRoller
+//
+//  Created by Bronson Mullens on 3/16/21.
+//
+
+import UIKit
+
+class DiceController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    func getAllItems() {
+        
+    }
+    
+    func createItem(name: String, sides: Int) {
+        
+    }
+    
+    func deleteItem(name: Dice) {
+        
+    }
+    
+    func updateItem(name: Dice) {
+        
+    }
+    
+}
