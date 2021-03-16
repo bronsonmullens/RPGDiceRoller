afd81c49dbc36413e822b3b568b0868a31e465b0
Bronson Mullens <bronsonmullens@icloud.com>
2021-03-16 12:41:48 -0700

    Added git ignore

--- RPGDiceRoller/View Controllers/DiceViewController.swift
+++ RPGDiceRoller/View Controllers/DiceViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class DiceViewController: UIViewController {
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         view.backgroundColor = .systemGray
         self.title = "Dice Roller"
