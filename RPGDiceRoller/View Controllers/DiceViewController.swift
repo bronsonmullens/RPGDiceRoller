568da3e3d3df68ffd6abac82b402d8ae15c52ff6
Bronson Mullens <bronsonmullens@icloud.com>
2021-03-16 10:09:01 -0700

    Set up project for programmatic UI and organized files.

---
+++ RPGDiceRoller/View Controllers/DiceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewController.swift
+//  RPGDiceRoller
+//
+//  Created by Bronson Mullens on 3/16/21.
+//
+
+import UIKit
+
+class DiceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .systemRed
+        
+    }
+    
+}
