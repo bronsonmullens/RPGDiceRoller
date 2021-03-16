f69644d2ea6e500ef865df54882fed4388cb38cb
Bronson Mullens <bronsonmullens@icloud.com>
2021-03-16 14:01:29 -0700

    Set up collection view with mock data.

--- RPGDiceRoller/View Controllers/DiceViewController.swift
+++ RPGDiceRoller/View Controllers/DiceViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class DiceViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    let diceReuseIdentifier = "DiceCell"
+    
+    lazy var diceCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: diceLayout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: diceReuseIdentifier)
+        return collectionView
+    }()
+    
+    let diceLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        layout.itemSize = CGSize(width: 96, height: 96)
+        return layout
+    }()
+    
+    // MOCK DATA
+    var diceBag = [4, 6, 8, 10, 12, 20, 100]
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        diceCollectionView.delegate = self
+        diceCollectionView.dataSource = self
         view.backgroundColor = .systemGray
-        self.title = "Dice Roller"
+        configureViews()
+    }
+    
+    // MARK: - Autolayout
+    
+    func configureViews() {
+        view.addSubview(diceCollectionView)
+        
+        diceCollectionView.register(DiceCell.self, forCellWithReuseIdentifier: diceReuseIdentifier)
+        
+        diceCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        diceCollectionView.backgroundColor = .none
+        
+        NSLayoutConstraint.activate([
+            diceCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            diceCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            diceCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            diceCollectionView.heightAnchor.constraint(equalToConstant: view.frame.height/2),
+        ])
+    }
+    
+}
+
+    // MARK: - Collection View Delegate & Data Source Methods
+
+extension DiceViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return diceBag.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: diceReuseIdentifier, for: indexPath) as? DiceCell else { return UICollectionViewCell() }
+        cell.diceLabel.text = "\(diceBag[indexPath.row])"
+        return cell
     }
     
 }
