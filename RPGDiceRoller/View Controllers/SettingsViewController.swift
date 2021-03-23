8592ee3c88eaebf18df1ae1c773f3f82958a3533
Bronson Mullens <bronsonmullens@icloud.com>
2021-03-22 21:12:17 -0700

    Set up settings vc

--- RPGDiceRoller/View Controllers/SettingsViewController.swift
+++ RPGDiceRoller/View Controllers/SettingsViewController.swift
@@ -9,6 +9,102 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    // MARK: - Properties
     
+    enum settings: String {
+        case whatsNew = "🗞 What's New?"
+        case twitter = "🐦 Twitter"
+        case tipJar = "💰 Tip Jar"
+        case rateTheApp = "⭐️ Rate the App"
+        case feedback = "🦻🏻 Feedback"
+        case helpfulTips = "🆘 Helpful Tips"
+        case privacyPolicy = "⚖️ Privacy Policy"
+        case deleteAllDice = "🗑 Delete All Dice"
+        case deleteAllMacros = "🗑 Delete All Macros"
+    }
+    
+    let sectionOne: [settings] = [
+        settings.whatsNew,
+        settings.twitter,]
+    let sectionTwo: [settings] = [
+        settings.tipJar,
+        settings.rateTheApp,
+        settings.feedback,
+        settings.helpfulTips,]
+    let sectionThree: [settings] = [
+        settings.deleteAllDice,
+        settings.deleteAllMacros,]
+    
+    let staticSettings: [[String]] = [
+        [settings.whatsNew.rawValue,
+         settings.twitter.rawValue],
+        
+        [settings.tipJar.rawValue,
+         settings.rateTheApp.rawValue,
+         settings.feedback.rawValue,
+         settings.helpfulTips.rawValue],
+        
+        [settings.deleteAllDice.rawValue,
+         settings.deleteAllMacros.rawValue],
+    ]
+    
+    lazy var numberOfRows = [sectionOne.count,
+                             sectionTwo.count,
+                             sectionThree.count,]
+    
+    let tableView = UITableView(frame: CGRect(), style: .grouped)
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Settings"
+        tableView.delegate = self
+        tableView.dataSource = self
+        configureViews()
+    }
+    
+    func configureViews() {
+        view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SettingCell")
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 128),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        view.backgroundColor = UIColor(named: "Background")
+        navigationController?.navigationBar.barTintColor = UIColor(named: "Foreground")
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        numberOfRows[section]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        staticSettings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath)
+        if let textLabel = cell.textLabel {
+            textLabel.text = staticSettings[indexPath.section][indexPath.row]
+            textLabel.textColor = .white
+            textLabel.font = .boldSystemFont(ofSize: 16)
+            cell.backgroundColor = UIColor(named: "Foreground")
+        }
+        return cell
+    }
     
 }
