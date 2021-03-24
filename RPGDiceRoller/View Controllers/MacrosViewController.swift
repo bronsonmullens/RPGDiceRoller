59f0aa0ee723cfc7281d8d688fb03f0642a757da
Bronson Mullens <bronsonmullens@icloud.com>
2021-03-23 21:57:25 -0700

    Stubbed out Macro VC.

--- RPGDiceRoller/View Controllers/MacrosViewController.swift
+++ RPGDiceRoller/View Controllers/MacrosViewController.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class MacrosViewController: UIViewController {
     
+    // MARK: - Properties
     
+    let diceController = DiceController.shared
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Macros"
+        configureViews()
+        configureColors()
+    }
+    
+    func configureViews() {
+        // TODO
+    }
+    
+    func configureColors() {
+        view.backgroundColor = UIColor(named: "Background")
+        navigationController?.navigationBar.barTintColor = UIColor(named: "Foreground")
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        navigationController?.navigationBar.tintColor = .white
+    }
     
 }
---
+++ RPGDiceRoller/Views/Macro/MacroCell.swift
@@ -0,0 +1,14 @@
+//
+//  MacroCell.swift
+//  RPGDiceRoller
+//
+//  Created by Bronson Mullens on 3/23/21.
+//
+
+import UIKit
+
+class MacroCell: UICollectionViewCell {
+    
+    
+    
+}
